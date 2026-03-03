31d315bc4ca1f01f32a0c6111eeed3215cc89f18
harsha1482004 <harsha1482004@gmail.com>
2026-03-03 15:42:43 +0530

    Comparision Operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,20 @@
+// Comparison operators
+console.log("Comparison operators");
+let x=10,y="10";
+console.log("x==y: ",x==y); //loose equality
+console.log("x===y: ",x===y); //strict equality
+console.log("x!=y: ",x!=y); //loose inequality
+console.log("x!==y: ",x!==y); //strict inequality
+console.log("x>y: ",x>y); 
+console.log("x<y: ",x<y);
+console.log("x>=y: ",x>=y);
+console.log("x<=y: ",x<=y);
+
+
+// Logical Operators
+console.log("Logical Operators");
+let a=false,b=false;
+console.log("a&&b=",a&&b); //AND
+console.log("a||b=",a||b); //OR 
+console.log("!a=",!a); //NOT
+
