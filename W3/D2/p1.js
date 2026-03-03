6cd01caef9bc284a50ea26464669b1d80d227373
harsha1482004 <harsha1482004@gmail.com>
2026-03-03 15:41:41 +0530

    Arithmetic Operators

---
+++ W3/D2/p1.js
@@ -0,0 +1,13 @@
+// Arithmetic Operators
+console.log("Arithmetic Operators");
+const a=2, b=3 ;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);
+console.log("a**b",a**b);
+// console.log("a++",a++);
+// console.log("++a",++a);
+// console.log("a--",a--);
+// console.log("--a",--a);
