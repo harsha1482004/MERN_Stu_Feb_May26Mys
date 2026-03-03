8056b1a02980298f0d3196e671f6288dbd4857d4
harsha1482004 <harsha1482004@gmail.com>
2026-03-03 15:46:03 +0530

    Fuctions declaration

---
+++ W3/D2/p5.js
@@ -0,0 +1,11 @@
+// Add tw0 number
+const Sum=(a,b) => {
+    return a+b;
+}
+console.log("3+5: ",Sum(3,5));
+
+
+// Single line return /implict retrn
+const square=x=> x*x;
+// console.log("Square of 44:",square(44));
+const sayHello=() => console.log("Hello from arrow func");
