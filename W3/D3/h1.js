bec4f75a652495085f8475a8a7f54492c6fa658a
harsha1482004 <harsha1482004@gmail.com>
2026-03-04 15:12:05 +0530

    for loop

---
+++ W3/D3/h1.js
@@ -0,0 +1,16 @@
+// // for loop
+// console.log("for loop examples");
+// for(let i=0;i<5;i++){
+//     console.log("i",i);
+// }
+
+// for(let i=5;i>0;i--){
+//     console.log("i",i);
+// }
+
+// Nested
+for(i=1;i<=3;i++){
+    for(j=0;j<=2;j++){
+        console.log(`i=${i},j=${j}`)
+    }
+}
