ce1ebf6be243b105586c495eab44af86f64a506f
harsha1482004 <harsha1482004@gmail.com>
2026-03-04 15:12:50 +0530

    while loop

---
+++ W3/D3/h2.js
@@ -0,0 +1,18 @@
+// while loop
+console.log("while loop");
+
+let count=0;
+while(count<5){
+    console.log('count:',count);
+    count--;
+}
+
+// do-while
+let value=0;
+do{
+    console.log("value:",value);
+    value++;
+}
+while(value<3)
+
+
