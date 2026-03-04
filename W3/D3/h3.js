eaf6b4d8851c309e400459ab16326992dbc5f663
harsha1482004 <harsha1482004@gmail.com>
2026-03-04 15:13:37 +0530

    break statement

---
+++ W3/D3/h3.js
@@ -0,0 +1,15 @@
+// break statement
+for(let i=0;i<5;i++){
+    if(i===2){
+        console.log("i",i);
+        break;
+    }  
+}
+
+// continue statement
+for(let i=0;i<5;i++){
+    if(i===2){
+        continue;
+    } 
+    console.log("i",i); 
+}
