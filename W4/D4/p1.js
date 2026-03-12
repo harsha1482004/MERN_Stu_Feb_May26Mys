c7ad88d3b0a775a2741103d6fdc36916afd0a978
harsha1482004 <harsha1482004@gmail.com>
2026-03-12 16:38:45 +0530

    Introduction to Encoding

---
+++ W4/D4/p1.js
@@ -0,0 +1,16 @@
+const inspectBtn=document.getElementById("inspectBtn");
+const out=document.getElementById("out");
+
+inspectBtn.addEventListener("click",function(){
+    const info={
+        htmlLang:document.documentElement.lang,
+        charset:document.characterSet,
+        //browser's primary preferred languages
+        browserLanguage:navigator.language, 
+        //array of all browser languages
+        browserLanguages:navigator.languages,
+        sampleText:document.getElementById('sampleText').textContent
+    };
+    console.log(info);
+    out.textContent=JSON.stringify(info,null,2)
+})
