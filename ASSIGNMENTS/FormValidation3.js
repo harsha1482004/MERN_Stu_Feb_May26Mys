f83508833412ea5c0bc5244c13842cb42b2cdbd4
harsha1482004 <harsha1482004@gmail.com>
2026-03-15 19:07:10 +0530

    Select FeedBack Type

---
+++ ASSIGNMENTS/FormValidation3.js
@@ -0,0 +1,8 @@
+const name=document.getElementById("name");
+const email=document.getElementById("email");
+const type=document.getElementById("type");
+const text=document.getElementById("text");
+const sumbit=document.getElementById("submit");
+const deleteBtn=document.getElementById("deleteBtn");
+const error=document.getElementById("error");
+
