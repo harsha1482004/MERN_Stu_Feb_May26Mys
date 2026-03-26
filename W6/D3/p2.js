1a7fcc2102afc59023afe451331348b6349ba403
harsha1482004 <harsha1482004@gmail.com>
2026-03-26 20:23:45 +0530

    // Introduction to the node js file system (fs) built in module.

---
+++ W6/D3/p2.js
@@ -0,0 +1,25 @@
+//Reading and Writing files synchronously
+
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname,"sync-note.txt")
+
+ fs.writeFileSync(filePath,"This file was written using writeFileSync().\nSynchronous operation block execution");
+//Append to a file 
+fs.appendFile(filePath,"  New text added using fs.appendFile.",
+    function(error){
+        if(error){
+            console.log("Append error:",error.message);
+            return;
+            
+        }
+         console.log("file content appended");
+    }
+)
+
+console.log("File written synchronously.");
+const content = fs.readFileSync(filePath,"utf-8");
+
+console.log("File read synchronously.");
+console.log("file content:",content);
