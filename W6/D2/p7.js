d5d82968353cd216533c282ccf52ef827bbca10d
harsha1482004 <harsha1482004@gmail.com>
2026-03-26 20:04:27 +0530

    Process.nextTick, Promise microtask & Timer

---
+++ W6/D2/p7.js
@@ -0,0 +1,13 @@
+// setImmediate vs setTimeout
+console.log("Scheduling setTimeout and setImmediate");
+// callback timer
+setTimeout(() => {
+    console.log("Timer callback from setTimeout");
+});
+
+// setImmediate also a callback 
+setImmediate(function(){
+    console.log("SetImmediate callback executed");
+});
+
+console.log("Both callbacks are now waiting for the event loop");
