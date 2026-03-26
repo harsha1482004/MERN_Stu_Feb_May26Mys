48b758d9f2cf7cc7ed7c3dd71665e33f835f7e51
harsha1482004 <harsha1482004@gmail.com>
2026-03-26 19:59:32 +0530

    How JS handles asynchronus tasks in node js

---
+++ W6/D2/p2.js
@@ -0,0 +1,13 @@
+// How JS handles asynchronus tasks in node js
+function fetchReport(callback){
+    console.log("Fetching report data...");
+
+    setTimeout(() => {
+        const report = "Monthly report is ready";
+        callback(report);
+    },1000);
+};
+fetchReport(function(reportMessage){
+    console.log(reportMessage);
+});
+console.log("Application continues to execute further");
