9267f57121cb35f15e55ca456a881c6133d086d9
harsha1482004 <harsha1482004@gmail.com>
2026-04-01 13:01:48 +0530

    Generating token using login function & verifing the token

---
+++ W7/D3/P6.js
@@ -0,0 +1,35 @@
+// Generating token using login function & verifing the token
+const jwt=require("jsonwebtoken");
+
+const secretKey="monkey123";
+
+function loginUser(email,password){
+    if(email==="correct@email.com"&&password==="mp123"){
+        const token=jwt.sign({
+            userId:101,
+            email:email,
+            role:"student"
+        },secretKey,{expiresIn:"1h"});
+        return{
+            success:true,
+            token:token
+        };
+    }
+    return{
+        success:false,
+        message:"Invalid credentials"
+    };
+};
+const loginResult=loginUser("correct@email.com","mp123");
+console.log("login result",loginResult);
+
+if(loginResult.success){
+    try {
+        const verifiedPayload=jwt.verify(loginResult.token,secretKey);
+            
+        console.log("Verified Payload",verifiedPayload)
+    } 
+    catch (error) {
+        console.log("Verification Failed",error.message);
+    }
+}
