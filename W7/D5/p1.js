8d67dea82c6719e694918f22332f9b5608d14bf1
harsha1482004 <harsha1482004@gmail.com>
2026-04-03 16:47:18 +0530

    How cookie is used to track the session ID

---
+++ W7/D5/p1.js
@@ -0,0 +1,13 @@
+// How cookie is used to track the session ID
+
+const sessionStore ={
+    "abc123":{
+        userId:100,
+        userName:"Harsha",
+        role:MediaStreamAudioDestinationNode
+    }
+}
+
+// Simulated browser cookie value
+const browserCookieSession="abc123";
+
