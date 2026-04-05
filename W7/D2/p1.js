c9c25ebe9b37c1797b1a9f0af6d0c76dc090b01e
harsha1482004 <harsha1482004@gmail.com>
2026-04-05 14:43:59 +0530

    Routing: Nested routes

---
+++ W7/D2/p1.js
@@ -0,0 +1,60 @@
+// Routing: Nested routes
+//  "/" base url
+//  "/api/users"
+
+const express = require("express");
+const app = express();
+
+// "/api/users" /create /delete /updtae /:id
+
+// Router objects help organize route Groups
+const apiRouter=express.Router();
+
+apiRouter.get("/users",function(req,res){
+    res.json({
+        route:"/api/users",
+        message:"Users route inside api router"
+    })
+});
+
+apiRouter.get("/orders",function(req,res){
+    res.json({
+        route:"/api/orders",
+        message:"Orders route inside api router"
+    })
+});
+
+// Mounting the router under /api base path
+app.use("/api",apiRouter);
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
+
+
+// ****New Router****
+// product routers (handles /api/products/...)
+const productsRouter=express.Router();
+
+// /api/products     -> create product
+productsRouter.post("/",(req,res) =>{
+    res.json({
+        route:"/api/products",
+        message:"Create products"
+    })
+});
+
+// /api/products/:id   -> delete product
+productsRouter.delete("/:id",(req,res) =>{
+    res.json({
+        route:"/api/products/${req.params.id}",
+        message:"Delete products"
+    })
+});
+
+// mount products router under /api/products
+apiRouter.use("/products",productsRouter);
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
