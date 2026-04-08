a6e50e5b4ff8089f36f08aa54b6c4c34109848c3
harsha1482004 <harsha1482004@gmail.com>
2026-04-08 12:36:41 +0530

    Genric handle middleware is created

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/errorMiddleware.js
@@ -0,0 +1,11 @@
+// Genric handle middleware is created
+
+function errorMiddleware(err,req,res,next){
+    const statusCode= err.statusCode || 500;
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Internal Server Error"
+    });
+}
+module.exports = errorMiddleware;
