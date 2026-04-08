b1be03db0e96eba4862bfe7554e87105221b9b8b
harsha1482004 <harsha1482004@gmail.com>
2026-04-08 11:31:00 +0530

    Middleware to check if user is authenticated or not

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+// Middleware to check if user is authenticated or not
+ 
+const jwt = require("jsonwebtoken");
+const CustomError = require("../utils/customError");
+
+const JWT_SECRET ="Thumba Secret";
+
+function authMiddleware(req,res,next){
+    try{
+        const authHeader = req.headers.authorization;
+        const tokenFromHeader = authHeader && authHeader.startsWith("Bearer ")?authHeader.split(" ")[1]: null;
+
+        const token= tokenFromHeader || req.cookies.token;
+
+        if(!token){
+            return next(new CustomError("Access denied. No token provided.",401));
+        }
+        const decoded = jwt.verify(token,JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch(error){
+        next(new CustomError("Invalid or expired token.",401));
+    }
+}
+
+module.exports = {
+    authMiddleware,
+    JWT_SECRET
+};
