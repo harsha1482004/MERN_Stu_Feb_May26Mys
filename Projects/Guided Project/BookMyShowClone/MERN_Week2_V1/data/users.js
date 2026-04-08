15026b6f2ae7315ac557215bee28f7c86183e980
harsha1482004 <harsha1482004@gmail.com>
2026-04-08 10:07:19 +0530

    User's details of the application.

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,19 @@
+// User's details of the application
+const users = [
+    {
+        id:1,
+        name:"Harsha Vardhan",
+        email:"harsha@e.com",
+        password:"mahi123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"admin",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    }
+];
+
+module.exports = users;
