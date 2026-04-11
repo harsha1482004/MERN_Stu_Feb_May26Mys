f6664587641804bfa33c117671d42543bf96494b
harsha1482004 <harsha1482004@gmail.com>
2026-04-11 11:44:39 +0530

    Indexing for performance improvement

---
+++ W8/D2/p2.js
@@ -0,0 +1,19 @@
+// Indexing for performance improvement
+
+const mongoose=require("mongoose");
+
+const employeeSchema=new mongoose.Schema({
+    email:{type:String, index:true},
+    username:{type:String,unique:true},
+    role:String,
+    createdAt:Date
+});
+
+// Compound Index
+// Query filter by role & sort by createdAt
+employeeSchema.index({role:1,createdAt:-1});
+
+const Employee=mongoose.model("IndexedEmployee",employeeSchema);
+
+console.log("Compound index: ");
+console.log("{role:1,createdAt:-1}");
