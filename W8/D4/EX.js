9159afae6ab231462cd6d744f9ec64e3c10303d1
harsha1482004 <harsha1482004@gmail.com>
2026-05-04 09:46:40 +0530

    Basics of embedding and referencing

---
+++ W8/D4/EX.js
@@ -0,0 +1,38 @@
+// Generating OTP using crypto & hashing it using bcrypt
+const crypto=require("crypto");
+const bcrypt=require("bcrypt");
+const mongoose=require("mongoose")
+
+try {
+    await mongoose.connect('mongodb://localhost:27017/datedb');
+    console.log("MongoDB connected");
+
+    function generateOTP(length = 6) {
+        return crypto.randomInt(100000, 999999).toString();
+    }
+
+    async function hashOTP(otp) {
+        return await bcrypt.hash(otp, 10)   // 10 indicates salt round 2^10
+    }
+
+    async function verifyOTP(input, hash) {
+        return await bcrypt.compare(input, hash);
+    }
+
+    (async () => {
+        const otp = generateOTP();
+        console.log("Generated otp: ", otp);
+
+        const hashedOtp = await hashOTP(otp);
+        console.log("Hashed otp", hashedOtp);
+
+        console.log("Verification: ", await verifyOTP(otp, hashedOtp));
+    })();
+}
+catch(error){
+    console.log("ERROR:",error.message)
+}
+finally {
+    await mongoose.disconnect();
+    console.log("Db disconnected");
+}
