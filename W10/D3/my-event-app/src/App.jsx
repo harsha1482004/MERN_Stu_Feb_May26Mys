f189159e4ee6516584cc71772f2f97e8a171d558
harsha1482004 <harsha1482004@gmail.com>
2026-05-07 12:30:47 +0530

    Add UseCallbackEvents component and update App to use it

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import {EventBasics} from "./components/P1"
 import {EventObject} from "./components/P2"
 import {PassingArguments} from "./components/P3"
+import {UseCallbackEvents} from "./components/P5"
 
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
       {/* <EventBasics/> */}
       {/* <EventObject/> */}
-      <PassingArguments/>
+      {/* <PassingArguments/> */}
+      <UseCallbackEvents/>
     </>
   )
 }
