0ca12d59321de81dc9fc878bd8779295bd433510
harsha1482004 <harsha1482004@gmail.com>
2026-06-13 13:07:00 +0530

    Add routing and dashboard components with protected routes

--- W12/Final Frontend Project/src/components/Navbar.jsx
+++ W12/Final Frontend Project/src/components/Navbar.jsx
@@ -1,7 +1,62 @@
+import {NavLink} from "react-router-dom";
 export default function Navbar(){
     return(
-        <nav className="navbar">
+        <nav style={StyleSheet.nav}>
             <h3>BookMyShow App</h3>
+            <div style={StyleSheet.links}>
+                <NavLink to="/"
+                end
+                style={getNavStyle}>
+                    Home
+                </NavLink>
+                <NavLink to="/movies"
+                
+                style={getNavStyle}>
+                    Movies
+                </NavLink>
+                <NavLink to="/login"
+                
+                style={getNavStyle}>
+                    Login
+                </NavLink>
+                <NavLink to="/signup"
+                style={getNavStyle}>
+                    Signup
+                </NavLink>
+                <NavLink to="/bookings"
+                
+                style={getNavStyle}>
+                    Bookings
+                </NavLink>
+                <NavLink to="/dashboard"
+                
+                style={getNavStyle}>
+                    Dashboard
+                </NavLink>
+            </div>
         </nav>
-    )
+    );
 }
+function getNavStyle(isActive){
+    return{
+        textDecoration: "none",
+        color: isActive? "#d32f2f":"#333",
+        fontWeight: isActive?"bold":"normal",
+        borderBottom: isActive?"2px solid #d32f2f":"none",
+        paddingBottom: "4px"
+    };
+}
+const styles = {
+    nav:{
+        display:"flex",
+        justfyContent:"space-between",
+        alignItems:"center",
+        padding:"15px 25px",
+        borderBottom:"1px solid #ddd",
+        marginBottom:"20px"
+    },
+    links:{
+        dispaly:"flex",
+        gap:"20px"
+    }
+};
